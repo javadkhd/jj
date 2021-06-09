46c9b838027029176b58e7013bf513cab8de8d54
javadkhd <j.khadem95@gmail.com>
2021-06-09 09:46:18 +0430

    send data to html page

--- nozad/icd10/views.py
+++ nozad/icd10/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from django.db import connection
+import sqlite3
+
+
+def sql_fetch(con):
+    cursorObj = con.cursor()
+
+    cursorObj.execute('SELECT * FROM Sheet۱')
+
+    rows = cursorObj.fetchall()
+
+    for row in rows[:10]:
+        print(row)
+    return rows[:10]
 
 
 def nozad(request):
     """
     This view will send data to page
     """
-    cursor = connection['nozad.db'].cursor()
 
-    col = cursor.execute("SELECT index FROM Sheet۱").fetchall()
+    con = sqlite3.connect('nozad.db')
 
+    rows = sql_fetch(con)
 
-    # cursor = connections['db.sqlite3'].cursor()
+    col = "111111111111"
 
-    # db = connection.
-    db_col_1 = col
+    db_col_1 = rows[:10]
     # db_col_2 =
     # db_col_3 =
     return render(
