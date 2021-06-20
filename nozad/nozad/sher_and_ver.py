f77ceba56f7445b293c7f8cf290ac59a28396d3b
javadkhd <j.khadem95@gmail.com>
2021-06-20 12:01:33 +0430

    before submit

---
+++ nozad/nozad/sher_and_ver.py
@@ -0,0 +1,38 @@
+import re
+
+sentence = "درمان سقط عفونی به کمک جراحی یا هر گونه سقط القایی و تخلیه‌ای در سه ماهه دوم بارداری با یا بدون مصرف دارو ( پروستاگلاندین) و یا وسیله (لامیناریا) با یا بدون کورتاژ شامل بستری در بیمارستان و ویزیت‌ها، زایمان جنین و جفت و پرده ها با دیلاتاسیون و کوتاژ و یا تخلیه"
+print(sentence)
+
+# new_sentences = sentence.split(" با یا ")
+# # print(new_sentences)
+# print(len(new_sentences))
+# for i in new_sentences:
+#     # j = i.replace(" یا ", " و ")
+#     # j = i.split(" یا ")
+#     print(i)
+#     print(j)
+
+# sentence = sentence.split(" با یا ")
+for i in sentence:
+
+    j = i.replace(" و یا ", "*")
+    j = i.replace(" و یا ", "*")
+    j = j.replace(" یا ", "*")
+    j = j.replace(" و ", "*")
+    j = j.split("*")
+    print(i)
+    print(j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
